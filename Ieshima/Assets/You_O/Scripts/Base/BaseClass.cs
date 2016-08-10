1325fcca05d9309484848e29e23ad51a72b679b9
You Oyadomari <gene_crew_lunch@yahoo.co.jp>
2016-08-11 00:30:29 +0900

    /BaseClass,FarmClassの作成

---
+++ Ieshima/Assets/You_O/Scripts/Base/BaseClass.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseClass
+{
+
+    protected int CheckNowMonth = 0;  //月チェック用変数
+
+    protected bool CheckChangeMonth()
+    {
+        if (CheckNowMonth == STATIC_SPACE.StaticValue.Month)
+        {
+            CheckNowMonth = STATIC_SPACE.StaticValue.Month;
+            return false;
+        }
+        else
+        {
+            CheckNowMonth = STATIC_SPACE.StaticValue.Month;
+            return true;
+        }
+    }
+
+    protected void PayCost(int cost)
+    {
+        STATIC_SPACE.StaticValue.NationalTreasury -= cost;
+    }
+
+    protected void Gain(int profit)
+    {
+        STATIC_SPACE.StaticValue.NationalTreasury = profit;
+    }
+}
---
+++ Ieshima/Assets/You_O/Scripts/Base/FarmClass.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FarmClass : BaseClass {
+
+    protected int ProduceGrowth = 0;
+    protected int HarvestDays;
+    protected bool FactoryFlag = false;
+
+    protected void GrowUp()
+    {
+        ProduceGrowth += UnityEngine.Random.Range(1, CONSTATIC_SPACE.ConstaticValue.MaximumGrowth);
+    }
+
+    protected bool CheckHarvestDay()
+    {
+        if(--HarvestDays <= 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    protected void Reset(int harvestdays)
+    {
+        HarvestDays = harvestdays;
+        ProduceGrowth = 0;
+    }
+}
