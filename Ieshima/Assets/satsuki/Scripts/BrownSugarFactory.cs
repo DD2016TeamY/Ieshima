3af3d64333bfe8da648ad3ea0392f2170e0c97b5
OretHub <satuki-o-15d@stu.kbc.ac.jp>
2016-08-25 12:26:36 +0900

    /コメント

--- Ieshima/Assets/satsuki/Scripts/BrownSugarFactory.cs
+++ Ieshima/Assets/satsuki/Scripts/BrownSugarFactory.cs
@@ -5,13 +5,21 @@ using CONSTATIC_SPACE;
 
 public class BrownSugarFactory : FactoryClass {
 
+    private int SugarcaneGrowth; //工場に入ってくるサトウキビ 
+
 	// Use this for initialization
 	void Start () {
-	    
+        SugarcaneGrowth = 0;
+        PayCost(CONSTATIC_SPACE.ConstaticValue.BrownSugarFactoryCost);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+
 	}
+
+
+
 }
