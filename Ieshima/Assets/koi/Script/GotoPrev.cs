305fe955b819e0a909fd606538e272c5e32f4aa3
KBC11506042\user <tatuhiko-u-15d@stu.kbc.ac.jp>
2016-09-06 10:43:11 +0900

    /ファイル名修正

---
+++ Ieshima/Assets/koi/Script/GotoPrev.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using STATIC_SPACE;
+using CONSTATIC_SPACE;
+
+public class GotoPrev : FactoryClass{
+    public GameObject PrevOage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+   void GotoNextPage(){
+    PrevOage.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}
