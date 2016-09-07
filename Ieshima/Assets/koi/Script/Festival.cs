b2f6ea2957ae741deef80f53fd71c7c1533384a1
KBC11506042\user <tatuhiko-u-15d@stu.kbc.ac.jp>
2016-09-07 10:06:47 +0900

    /ファイル名修正

---
+++ Ieshima/Assets/koi/Script/Festival.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using STATIC_SPACE;
+using CONSTATIC_SPACE;
+
+public class Festival : BaseClass {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void FixedUpdate(){
+        if (CheckChangeMonth()) {
+            STATIC_SPACE.StaticValue.EventsScale = 1;
+            if (STATIC_SPACE.StaticValue.Month == 4) {
+                STATIC_SPACE.StaticValue.EventsScale = 2;
+            }else if ( STATIC_SPACE.StaticValue.Month == 5){
+            STATIC_SPACE.StaticValue.EventsScale = 3;
+            }
+            else if(STATIC_SPACE.StaticValue.Month == 6){
+                STATIC_SPACE.StaticValue.EventsScale =4;
+            }
+        }       
+     }
+   }
