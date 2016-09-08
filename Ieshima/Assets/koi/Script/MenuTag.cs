0f3462fed921553193170098f2fcd12a4e9f2449
KBC11506042\user <tatuhiko-u-15d@stu.kbc.ac.jp>
2016-09-08 12:59:31 +0900

    /ファイル名修正

---
+++ Ieshima/Assets/koi/Script/MenuTag.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using CONSTATIC_SPACE;
+using STATIC_SPACE;
+
+public class MenuTag : FactoryClass{
+    public GameObject Parents;
+    public GameObject Child;
+ void ChangeTag(){
+ Parents 
+ 
+ }
+}
