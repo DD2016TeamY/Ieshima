1b5d5a5eec1d707eed1c9eeeaab7c05e39a0e522
You Oyadomari <gene_crew_lunch@yahoo.co.jp>
2016-09-21 16:17:57 +0900

    /a

---
+++ Ieshima/Assets/You_O/Scripts/Test/LinkSugarandBrawn.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinkSugarandBrawn : MonoBehaviour {
+    public bool LinkFlag;
+
+	// Use this for initialization
+	void Start () {
+        LinkFlag = false;
+	}
+
+    void Update()
+    {
+    }
+
+    void OnTriggerStay(Collider factory)
+    {
+        Debug.Log(factory.gameObject.tag);
+        Debug.Log(LinkFlag);
+
+        if (LinkFlag == false && factory.gameObject.tag == "BrawnSugarFactroy")
+        {
+            Debug.Log("Hello, world!");
+
+            LinkFlag = true;
+
+            this.transform.parent.GetComponent<sugarcane>().BrawnSugarFactory = factory.transform.parent.gameObject;
+
+            this.transform.parent.GetComponent<sugarcane>().FactoryFlag = true;
+        }
+    }
+}
